52f2bfe9dcefe44930d6709a8ae8bfa315d890d3
Jns Hul <jens.hulteberg@gmail.com>
2025-01-25 22:20:24 +0100

    Stacked menues

--- project/scenes/opening_crawl.gd
+++ project/scenes/opening_crawl.gd
@@ -9,8 +9,8 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	instance_start_menu()
 
 func instance_start_menu():
-	var start_menu = start_menu_res.instantiate()
-	get_parent().add_child(start_menu)
+	#var start_menu = start_menu_res.instantiate()
+	#get_parent().add_child(start_menu)
 	queue_free()
 
 func _input(event):
