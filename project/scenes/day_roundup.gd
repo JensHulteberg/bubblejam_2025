9785343c82d9a3f2a9bcc00b211f3a738ea8cd06
Jns Hul <jens.hulteberg@gmail.com>
2025-01-25 21:17:28 +0100

    new warnings for days

--- project/scenes/day_roundup.gd
+++ project/scenes/day_roundup.gd
@@ -3,16 +3,17 @@ extends Control
 func _ready() -> void:
 	set_process(false)
 
-func init(day_num: int, cash: int, stocks_value: int):
+func init(day_num: int, cash: int, stocks_value: int, licence_fee: int):
 	$CenterContainer/VBoxContainer/Label.text = "MARKET CLOSING DAY " + str(day_num)
 	$CenterContainer/VBoxContainer/HBoxContainer/Label2.text = str(cash)
 	$CenterContainer/VBoxContainer/HBoxContainer2/Label2.text = str(stocks_value)
-	$CenterContainer/VBoxContainer/HBoxContainer4/Label2.text = str(cash + stocks_value)
+	$CenterContainer/VBoxContainer/HBoxContainer4/Label2.text = str(cash + stocks_value - licence_fee)
+	$CenterContainer/VBoxContainer/HBoxContainer3/Label2.text = "- " + str(licence_fee)
 	$CenterContainer/VBoxContainer/Label.visible_characters = 0
 	$CenterContainer/VBoxContainer/HBoxContainer/Label2.visible_characters = 0
 	$CenterContainer/VBoxContainer/HBoxContainer2/Label2.visible_characters = 0
 	$CenterContainer/VBoxContainer/HBoxContainer4/Label2.visible_characters = 0
-	
+	$CenterContainer/VBoxContainer/HBoxContainer3/Label2.visible_characters = 0
 
 func trigger_text():
 	set_process(true)
@@ -22,6 +23,10 @@ func _process(delta: float) -> void:
 	$CenterContainer/VBoxContainer/HBoxContainer/Label2.visible_characters += 1
 	$CenterContainer/VBoxContainer/HBoxContainer2/Label2.visible_characters += 1
 	$CenterContainer/VBoxContainer/HBoxContainer4/Label2.visible_characters += 1
+
+func fail_to_pay():
+	$AnimationPlayer.play("overdue_warning")
+	await $AnimationPlayer.animation_finished
 
 func fade_in():
 	$AnimationPlayer.play("fade_in")
