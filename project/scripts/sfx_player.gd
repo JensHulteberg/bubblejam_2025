1e03e6e26458b9953d449df4ba7a3120a677c8c4
Fabian Sörensson <fabian.sorensson@gmail.com>
2025-01-25 23:37:30 +0100

    Add more SFX

--- project/scripts/sfx_player.gd
+++ project/scripts/sfx_player.gd
@@ -13,12 +13,16 @@ extends Node
 	"yeah": preload("res://sound/yeah.ogg"),
 	"gunshot": preload("res://sound/gunshot.ogg"),
 	"oh_no": preload("res://sound/oh_no.ogg"),
+	"woosh": preload("res://sound/woosh.ogg"),
+	"big_ben": preload("res://sound/big_ben.ogg"),
+	"cluster": preload("res://sound/cluster.ogg"),
 }
 
-func play_sfx(name: String):
+func play_sfx(name: String, pitch: float = 1.0):
 	var audio_stream_player = AudioStreamPlayer.new()
 	add_child(audio_stream_player)
 	audio_stream_player.stream = sound_effects[name]
+	audio_stream_player.pitch_scale = pitch
 	audio_stream_player.play()
 	await audio_stream_player.finished
 	audio_stream_player.queue_free()
