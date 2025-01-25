58d0ecf9178cb965c998376ff474ad6164c37799
Fabian Sörensson <fabian.sorensson@gmail.com>
2025-01-25 08:31:58 +0100

    Add basic card

---
+++ project/scripts/card.gd
@@ -0,0 +1,21 @@
+@tool
+extends Control
+
+@export var card_title: String = "CARD"
+@export var image: Texture2D = preload("res://graphics/placeholder_card_image.png")
+@export var description: String
+
+@onready var card_title_label: RichTextLabel = $MarginContainer/VBoxContainer/MarginContainer/CardTitleLabel
+@onready var image_texture_rect: TextureRect = $MarginContainer/VBoxContainer/MarginContainer2/ImageTextureRect
+
+
+func _ready():
+	_sync()
+
+func _process(delta: float):
+	if Engine.is_editor_hint():
+		_sync()
+
+func _sync():
+	card_title_label.text = "[center]" + card_title + "[/center]"
+	image_texture_rect.texture = image
