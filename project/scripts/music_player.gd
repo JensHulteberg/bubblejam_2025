e9c29e8a8a03919e308d7ffddfeac68e3d292ddc
Fabian Sörensson <fabian.sorensson@gmail.com>
2025-01-25 21:48:40 +0100

    Add intro music

---
+++ project/scripts/music_player.gd
@@ -0,0 +1,20 @@
+extends AudioStreamPlayer
+
+var songs = {
+	"theme": preload("res://sound/gloomberg.ogg"),
+	"intro": preload("res://sound/gloomberg_intro.ogg"),
+}
+
+func _ready() -> void:
+	play_song("intro")
+
+
+func play_song(name: String):
+	if playing:
+		var tween = create_tween()
+		tween.tween_property(self, "volume_db", -100, 1)
+		await tween.finished
+		stop()
+	volume_db = 0
+	stream = songs[name]
+	play()
