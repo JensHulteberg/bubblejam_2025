e1a24cfa5aada0111c8b5af5ca7beab1cb6561b9
dbergmarklund <david.bergmarklund@recordedfuture.com>
2025-01-25 13:35:24 +0100

    Multicolor stock graph

--- project/scripts/stock_graph.gd
+++ project/scripts/stock_graph.gd
@@ -5,7 +5,6 @@ var height: float = 91
 var aktie: Aktie 
 
 func set_stock(_aktie: Aktie) -> void:
-	
 	aktie = _aktie
 	queue_redraw()
 
@@ -15,8 +14,7 @@ func _draw() -> void:
 		return
 	var step_len =  width / aktie.history.size()
 	var height_div = aktie.min_max_values().y / height
-	var aktie_line = _aktie_to_Vector2Array(aktie, step_len, height_div)
-	draw_polyline(aktie_line, Color.GREEN, 1.0)
+	draw_aktie(aktie, step_len, height_div)
 	
 	
 func draw_border() -> void:
@@ -29,14 +27,29 @@ func draw_border() -> void:
 	], Color.WHITE, 1.0)
 
 
-func _aktie_to_Vector2Array(a: Aktie, step_len: float, height_div: float) -> PackedVector2Array:
+func draw_aktie(a: Aktie, step_len: float, height_div: float) -> void:
 	var coords: PackedVector2Array = []
-	var idx: int = 0;
+	var colors: PackedColorArray = []
+	var idx: int = 0
+	var last_value: int
 	for h in a.history:
 		coords.append(give_vector(h.value, idx, step_len, height_div))
+		if last_value != null:
+			colors.append(get_color(h.value, last_value))
 		idx += 1
+		last_value = h.value
+		
 	coords.append(give_vector(a.value, idx, step_len, height_div))
-	return coords
+	colors.append(get_color(a.value, last_value))
+	draw_polyline_colors(coords, colors, 1.0)
+	
+
+func get_color(value: int, last_value: int) -> Color:
+	if last_value < value:
+		return Color.GREEN
+	else:
+		return Color.RED
+
 
 func give_vector(value: int, idx: int, step_len: float, height_div: float) -> Vector2:
 	return Vector2(idx * step_len, -(value / height_div) + height)
