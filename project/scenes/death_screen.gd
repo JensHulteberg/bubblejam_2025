afc28c367bbea818bae3d02ab7b3bf09d0c7fd5f
Jns Hul <jens.hulteberg@gmail.com>
2025-01-26 11:21:57 +0100

    Cool death screen with music

--- project/scenes/death_screen.gd
+++ project/scenes/death_screen.gd
@@ -6,6 +6,7 @@ var loop_count = 0
 func _ready() -> void:
 	$AnimationPlayer.animation_finished.connect(_on_anim_finished)
 	$AnimationPlayer.play("death_anim_2")
+	SfxPlayer.play_sfx("cluster")
 	MusicPlayer.stop()
 	
 
@@ -14,16 +15,23 @@ func _on_anim_finished(anim_name):
 	if loop_count > max_loops - 2:
 		$CenterContainer/RichTextLabel.text = "YOU BLOW YOUR BRAINS OUT..."
 	if loop_count > max_loops:
-		visible = false
+		pass
+		#visible = false
 		
 		return
 	if loop_count > max_loops - 1:
 		$Timer.start()
 	$AnimationPlayer.play("death_anim_2")
+	SfxPlayer.play_sfx("cluster")
 
+func play_end_music():
+	MusicPlayer.play_song("end")
 
 func _on_timer_timeout() -> void:
 	SfxPlayer.play_sfx("gunshot")
-	$Timer.start(3)
-	await $Timer.timeout
-	get_tree().quit()
+	$logo_wait_timer.start(0.6)
+	await $logo_wait_timer.timeout
+	$AnimationPlayer.play("fade_in_logo")
+	
+	#get_tree().quit()
+	
