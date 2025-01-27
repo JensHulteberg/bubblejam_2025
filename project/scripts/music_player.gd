acd2272891166cb4c3bac182b5050f36d3b38776
Fabian Sörensson <fabian.sorensson@gmail.com>
2025-01-27 17:02:07 +0100

    Revert "Change how music loops"
    This reverts commit 8639f26543f17bc528adc0a949098224c4522b78.

--- project/scripts/music_player.gd
+++ project/scripts/music_player.gd
@@ -1,28 +1,19 @@
 extends AudioStreamPlayer
 
 var songs = {
-	"theme": {
-		"song": preload("res://sound/gloomberg.ogg"),
-		"loop_offset": 16.0,
-	},
-	"intro": {
-		"song": preload("res://sound/gloomberg_intro.ogg"),
-		"loop_offset": 0.0,
-	},
-	"end": {
-		"song": preload("res://sound/gloomberg_end.ogg"),
-		"loop_offset": 25.585,
-	},
+	"theme": preload("res://sound/gloomberg.ogg"),
+	"intro": preload("res://sound/gloomberg_intro.ogg"),
+	"end": preload("res://sound/gloomberg_end.ogg"),
 }
 
-var loop_offset = 0.0
-
+func _ready() -> void:
+	pass
+	#play_song("intro")
 
 func play_song(name: String):
 	await fade_out()
 	volume_db = 0
-	stream = songs[name].song
-	loop_offset = songs[name].loop_offset
+	stream = songs[name]
 	play()
 
 
@@ -32,7 +23,4 @@ func fade_out(duration: float = 1.0):
 		tween.tween_property(self, "volume_db", -100, duration)
 		await tween.finished
 		stop()
-
-
-func _on_finished() -> void:
-	play(loop_offset)
+	
